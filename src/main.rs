48e1c5ba7debf498af2b1d714347fef7a02c8635
Norman Kern <norman.kern@gmx.com>
2021-05-21 00:27:59 +0800

    Add help messages of blktrace

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
-fn main() {
-    let matches = App::new("Block Tracer")
-        .version("0.1.1")
+fn handle_opts<'a>() -> ArgMatches<'a> {
+    let blktrace_about = format!(
+        "\n{} \n\t{}",
+        "THIS IS A RUST VERSION OF BLKTRACE WRITEN BY", "Jens Axboe <axboe@kernel.dk>"
+    );
+
+    let matches = App::new("Block IO Tracing")
+        .version("2.0.0")
         .author("Norman Kern <norman.kern@gmx.com>")
-        .about("This is a rust version of blktrace.")
+        .about(&blktrace_about[..])
         .arg(
             Arg::with_name("dev")
                 .short("d")
@@ -33,5 +38,82 @@ fn main() {
                 .takes_value(true)
                 .help("Directory to prepend to output file names"),
         )
+        .arg(
+            Arg::with_name("time")
+                .short("w")
+                .long("stopwatch")
+                .takes_value(true)
+                .help("Stop after defined time, in seconds"),
+        )
+        .arg(
+            Arg::with_name("action field")
+                .short("a")
+                .long("act-mask")
+                .takes_value(true)
+                .help("Only trace specified actions. See documentation"),
+        )
+        .arg(
+            Arg::with_name("action mask")
+                .short("A")
+                .long("set-mask")
+                .takes_value(true)
+                .help("Give trace mask as a single value. See documentation"),
+        )
+        .arg(
+            Arg::with_name("size")
+                .short("b")
+                .long("buffer-size")
+                .takes_value(true)
+                .help("Sub buffer size in KiB (default 512)"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("num-sub-buffers")
+                .takes_value(true)
+                .help("Number of sub buffers (default 4)"),
+        )
+        .arg(
+            Arg::with_name("listen")
+                .short("l")
+                .long("listen")
+                .takes_value(false)
+                .help("Run in network listen mode (blktrace server)"),
+        )
+        .arg(
+            //NOTE: The C version of blktrace uses -h not -H.
+            Arg::with_name("hostname")
+                .short("H")
+                .long("host")
+                .takes_value(true)
+                .help("Run in network client mode, connecting to the given host"),
+        )
+        .arg(
+            Arg::with_name("port number")
+                .short("p")
+                .long("port")
+                .takes_value(true)
+                .help("Network port to use (default 8462)"),
+        )
+        .arg(
+            Arg::with_name("no-sendfile")
+                .short("s")
+                .long("no-sendfile")
+                .takes_value(false)
+                .help("Make the network client NOT use sendfile() to transfer data"),
+        )
+        .arg(
+            Arg::with_name("devs file")
+                .short("I")
+                .long("input-devs")
+                .takes_value(true)
+                .help("Add devices found in <devs file>"),
+        )
         .get_matches();
+
+    matches
+}
+
+fn main() {
+    handle_opts();
 }
